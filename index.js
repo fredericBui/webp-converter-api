94a27bbfddd695c8246717f41bf30ae5bb545e8d
Frédéric Bui <bui.frederic.pro@gmail.com>
2025-02-23 00:00:28 +0100

    :feat: try only to display a page

--- index.js
+++ index.js
@@ -7,31 +7,10 @@ const webp = require('webp-converter');
 const app = express();
 const port = 3000;
 
-const upload = multer({ dest: 'uploads/' });
-
 app.get('/', (req, res) => {
   res.write('<h1>Send file to /upload to convert your file to webp format</h1>');
 }
 );
-
-app.post('/upload', upload.single('file'), (req, res) => {
-  const filePath = path.join(__dirname, req.file.path);
-  const output = path.join(__dirname, req.file.path + '.webp');
-  const result = webp.cwebp(filePath, output, "-q 80", logging = "-v");
-  result.then((response) => {
-    console.log(response);
-    res.download(output);
-    fs.rm(filePath, function (err) {
-      if (err) throw err;
-      console.log('Successfully deleted the uploaded file');
-    });
-    fs.rm(output, function (err) {
-      if (err) throw err;
-      console.log('Successfully deleted the converted file');
-    }
-    );
-  });
-});
 
 app.listen(port, () => {
   console.log(`Server started on http://localhost:${port}`);
